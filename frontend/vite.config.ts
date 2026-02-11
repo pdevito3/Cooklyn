b1c76bd4a20540077017f7a76f5a5ddb84443936
Paul DeVito <pdevito3@gmail.com>
2026-02-11 11:51:03 -0500

    build: click to open component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
+process.env.LAUNCH_EDITOR = 'code'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +20,7 @@ export default defineConfig({
         exportType: 'default',
       },
     }),
+    reactClickToComponent(),
   ],
   resolve: {
     alias: {
