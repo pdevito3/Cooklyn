c97a3e8040907673f4d52877a7a70ac975bdc907
Paul DeVito <pdevito3@gmail.com>
2026-02-11 22:12:17 -0500

    feat: kbd cleanup

--- frontend/src/components/ui/kbd.tsx
+++ frontend/src/components/ui/kbd.tsx
@@ -1,7 +1,7 @@
 export function Kbd({ children }: { children: React.ReactNode }) {
   return (
-    <kbd className="pointer-events-none ml-1.5 hidden select-none items-center rounded border bg-muted/50 px-1 font-mono text-[10px] font-medium text-muted-foreground sm:inline-flex">
+    <kbd className="pointer-events-none ml-2 hidden shrink-0 select-none items-center justify-center rounded border bg-muted/50 px-1.5 py-0.5 font-sans text-xs font-semibold text-muted-foreground sm:inline-flex">
       {children}
     </kbd>
-  )
+  );
 }
